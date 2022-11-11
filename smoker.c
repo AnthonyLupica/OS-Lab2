0abfde47a9d5fa09bb4a5281da7af70167bccfd1
Anthony R. Lupica <arl127@uakron.edu>
2022-11-11 15:44:52 -0500

    Final version for submission

--- smoker.c
+++ smoker.c
@@ -5,11 +5,25 @@
     pthreads and semaphores 
 
     The Cigarette Smokers’ Problem 
+
+    How to Compile and Run:
+    In Ubuntu, or another Linux distribution, create a build directory in the folder containing 
+    the .c program and CMakeLists.txt. From within the build directory, type... 
+        
+        cmake ..
+
+    to create your Make files, followed by 
+        
+        make run 
+           or 
+        ./smoker 
+
+    to execute the program.
 */
 
 #include <stdio.h>      // printf     
-#include <pthread.h>
-#include <semaphore.h> 
+#include <pthread.h>    // pthreads
+#include <semaphore.h>  // semaphores
 #include <time.h>       // time 
 #include <stdlib.h>     // rand and srand
 #include <unistd.h>     // usleep
@@ -28,6 +42,9 @@ void *pushMatch(void *param);
 void *smokerWithTobaccoOne(void *param);
 void *smokerWithPaperOne(void *param);
 void *smokerWithMatchOne(void *param);
+void *smokerWithTobaccoTwo(void *param);
+void *smokerWithPaperTwo(void *param);
+void *smokerWithMatchTwo(void *param);
 
 /* Semaphores */
 
@@ -63,7 +80,7 @@ int main ()
     printf("|                                     |\n");
     printf(" - - - - - - - - - - - - - - - - - - - \n\n");
 
-    /* Initialize Semaphores */
+    /* INITIALIZE SEMAPHORES */
 
     sem_init(&agentSem, 0, 1);
     sem_init(&tobacco, 0, 0);
@@ -75,51 +92,64 @@ int main ()
     sem_init(&matchSem, 0, 0);
     sem_init(&mutex, 0, 1);
 
-    /* Declare Threads */
+    /* DECLARE THREADS */
 
-    // agent threads
+    // 3 agent threads
     pthread_t tobaccoPaper, tobaccoMatch, paperMatch;
     
-    // pusher threads
+    // 3 pusher threads
     pthread_t tobaccoPush, paperPush, matchPush;
 
-    // smoker threads
-    pthread_t tobaccoSmoker, paperSmoker, matchSmoker;
+    // 6 smoker threads
+    pthread_t tobaccoSmokerOne, paperSmokerOne, matchSmokerOne;
+    pthread_t tobaccoSmokerTwo, paperSmokerTwo, matchSmokerTwo;
 
-    // spawn agent threads 
+    /* SPAWN THREADS */
+
+    // spawn 3 agent threads 
     pthread_create(&tobaccoPaper, NULL, agent_tobaccoPaper, NULL);
     pthread_create(&tobaccoMatch, NULL, agent_tobaccoMatch, NULL);
     pthread_create(&paperMatch, NULL, agent_paperMatch, NULL);
 
-    // spawn pusher threads
+    // spawn 3 pusher threads
     pthread_create(&tobaccoPush, NULL, pushTobacco, NULL);
     pthread_create(&paperPush, NULL, pushPaper, NULL);
     pthread_create(&matchPush, NULL, pushMatch, NULL);
 
-    // spawn smoker threads
-    pthread_create(&tobaccoSmoker, NULL, smokerWithTobaccoOne, NULL);
-    pthread_create(&paperSmoker, NULL, smokerWithPaperOne, NULL);
-    pthread_create(&matchSmoker, NULL, smokerWithMatchOne, NULL);
+    // spawn 6 smoker threads
+    pthread_create(&tobaccoSmokerOne, NULL, smokerWithTobaccoOne, NULL);
+    pthread_create(&paperSmokerOne, NULL, smokerWithPaperOne, NULL);
+    pthread_create(&matchSmokerOne, NULL, smokerWithMatchOne, NULL);
+    pthread_create(&tobaccoSmokerTwo, NULL, smokerWithTobaccoTwo, NULL);
+    pthread_create(&paperSmokerTwo, NULL, smokerWithPaperTwo, NULL);
+    pthread_create(&matchSmokerTwo, NULL, smokerWithMatchTwo, NULL);
     
-    // join smokers
-    pthread_join(tobaccoSmoker, NULL);
-    pthread_join(paperSmoker, NULL);
-    pthread_join(matchSmoker, NULL);
+    /* JOIN THREADS */
 
-    // join agents
+    // join 6 smokers
+    pthread_join(tobaccoSmokerOne, NULL);
+    pthread_join(paperSmokerOne, NULL);
+    pthread_join(matchSmokerOne, NULL);
+    pthread_join(tobaccoSmokerTwo, NULL);
+    pthread_join(paperSmokerTwo, NULL);
+    pthread_join(matchSmokerTwo, NULL);
+
+    // join 3 pushers
+    pthread_join(tobaccoPush, NULL);
+    pthread_join(paperPush, NULL);
+    pthread_join(matchPush, NULL);
+
+    // join 3 agents
     pthread_join(tobaccoPaper, NULL);
     pthread_join(tobaccoMatch, NULL);
     pthread_join(paperMatch, NULL);
 
-    // join pushers
-    pthread_join(tobaccoPush, NULL);
-    pthread_join(paperPush, NULL);
-    pthread_join(matchPush, NULL);
-    
+    printf("\nAll 6 smokers have been satisfied. One question remains... at what cost?\n\n");
+
     return 0;
 }
 
-/* The agent tables tobacco and a rolling paper */
+/* agent one tables tobacco and a rolling paper */
 void *agent_tobaccoPaper(void *param)
 {
     for (int i = 0; i < 6; ++i) 
@@ -134,7 +164,7 @@ void *agent_tobaccoPaper(void *param)
     pthread_exit(0);
 }
 
-/* The agent tables tobacco and a match */
+/* agent two tables tobacco and a match */
 void *agent_tobaccoMatch(void *param)
 {
     for (int i = 0; i < 6; ++i) 
@@ -149,7 +179,7 @@ void *agent_tobaccoMatch(void *param)
     pthread_exit(0);
 }
 
-/* The agent tables a rolling paper and a match */
+/* agent three tables a rolling paper and a match */
 void *agent_paperMatch(void *param)
 {
     for (int i = 0; i < 6; ++i) 
@@ -164,6 +194,7 @@ void *agent_paperMatch(void *param)
     pthread_exit(0);
 }
 
+/* pusher one wakes up when tobacco is on the table */
 void *pushTobacco(void *param)
 {
     for (int i = 0; i < 12; ++i) 
@@ -173,11 +204,13 @@ void *pushTobacco(void *param)
 
         if (isPaper) 
         {
+            printf("\nAttention all MATCH smokers, TOBACCO and a PAPER are on the table!\n\n\n");
             isPaper = 0;
             sem_post(&matchSem);
         }
         else if (isMatch) 
         {
+            printf("\nAttention all PAPER smokers, TOBACCO and a MATCH are on the table!\n\n\n");
             isMatch = 0;
             sem_post(&paperSem);
         }
@@ -192,6 +225,7 @@ void *pushTobacco(void *param)
     pthread_exit(0); 
 }
 
+/* pusher two wakes up when a rolling paper is on the table */
 void *pushPaper(void *param)
 {
     for (int i = 0; i < 12; ++i) 
@@ -201,11 +235,13 @@ void *pushPaper(void *param)
 
         if (isTobacco) 
         {
+            printf("\nAttention all MATCH smokers, a PAPER and TOBACCO are on the table!\n\n\n");
             isTobacco = 0;
             sem_post(&matchSem);
         }
         else if (isMatch) 
         {
+            printf("\nAttention all TOBACCO smokers, a PAPER and a MATCH are on the table!\n\n\n");
             isMatch = 0;
             sem_post(&tobaccoSem);
         }
@@ -220,6 +256,7 @@ void *pushPaper(void *param)
     pthread_exit(0); 
 }
 
+/* pusher three wakes up when a match is on the table */
 void *pushMatch(void *param)
 {
     for (int i = 0; i < 12; ++i) 
@@ -229,11 +266,13 @@ void *pushMatch(void *param)
 
         if (isTobacco) 
         {
+            printf("\nAttention all PAPER smokers, a MATCH and TOBACCO are on the table!\n\n\n");
             isTobacco = 0;
             sem_post(&paperSem);
         }
         else if (isPaper) 
         {
+            printf("\nAttention all TOBACCO smokers, a MATCH and a PAPER are on the table!\n\n\n");
             isPaper = 0;
             sem_post(&tobaccoSem);
         }
@@ -248,6 +287,7 @@ void *pushMatch(void *param)
     pthread_exit(0); 
 }
 
+/* the first smoker with infinite tobacco */
 void *smokerWithTobaccoOne(void *param)
 {
     int smokeCount = 0;
@@ -259,19 +299,45 @@ void *smokerWithTobaccoOne(void *param)
         /* Make a cigarette */
         usleep(rand() % 50000 + 1);
         ++smokeCount;
-        printf("Smoker one with INFINITE TOBACCO rolled up blunt number %d\n\n", smokeCount);
+        printf("Smoker ONE with INFINITE TOBACCO rolled up blunt number %d\n\n", smokeCount);
         
         sem_post(&agentSem);
         
         /* Smoke the cigarette */
         usleep(rand() % 50000 + 1);
-        printf("Smoker one with INFINITE TOBACCO is blowing smoke on blunt number %d\n\n", smokeCount);
+        printf("Smoker ONE with INFINITE TOBACCO is blowing smoke on blunt number %d\n\n", smokeCount);
     }
 
-    printf("Smoker one with INFINITE TOBACCO got hungry and went home\n\n");
+    printf("Smoker ONE with INFINITE TOBACCO got hungry and went home\n\n");
     pthread_exit(0);
 }
 
+/* the second smoker with infinite tobacco */
+void *smokerWithTobaccoTwo(void *param)
+{
+    int smokeCount = 0;
+    
+    while (smokeCount < 3) 
+    {
+        sem_wait(&tobaccoSem);
+        
+        /* Make a cigarette */
+        usleep(rand() % 50000 + 1);
+        ++smokeCount;
+        printf("Smoker TWO with INFINITE TOBACCO rolled up blunt number %d\n\n", smokeCount);
+        
+        sem_post(&agentSem);
+        
+        /* Smoke the cigarette */
+        usleep(rand() % 50000 + 1);
+        printf("Smoker TWO with INFINITE TOBACCO is blowing smoke on blunt number %d\n\n", smokeCount);
+    }
+
+    printf("Smoker TWO with INFINITE TOBACCO got hungry and started snoring\n\n");
+    pthread_exit(0);
+}
+
+/* the first smoker with infinite paper */
 void *smokerWithPaperOne(void *param)
 {
     int smokeCount = 0;
@@ -283,19 +349,45 @@ void *smokerWithPaperOne(void *param)
         /* Make a cigarette */
         usleep(rand() % 50000 + 1);
         ++smokeCount;
-        printf("Smoker one with INFINITE PAPER rolled up blunt number %d\n\n", smokeCount);
+        printf("Smoker ONE with INFINITE PAPER rolled up blunt number %d\n\n", smokeCount);
     
         sem_post(&agentSem);
        
         /* Smoke the cigarette */
         usleep(rand() % 50000 + 1);
-        printf("Smoker one with INFINITE PAPER is blowing smoke on blunt number %d\n\n", smokeCount);
+        printf("Smoker ONE with INFINITE PAPER is blowing smoke on blunt number %d\n\n", smokeCount);
     }
 
-    printf("Smoker one with INFINITE PAPER got hungry and went to Taco Bell\n\n");
+    printf("Smoker ONE with INFINITE PAPER got hungry and went to Taco Bell\n\n");
     pthread_exit(0);
 }
 
+/* the second smoker with infinite paper */
+void *smokerWithPaperTwo(void *param)
+{
+    int smokeCount = 0;
+
+    while (smokeCount < 3) 
+    {
+        sem_wait(&paperSem);
+        
+        /* Make a cigarette */
+        usleep(rand() % 50000 + 1);
+        ++smokeCount;
+        printf("Smoker TWO with INFINITE PAPER rolled up blunt number %d\n\n", smokeCount);
+    
+        sem_post(&agentSem);
+       
+        /* Smoke the cigarette */
+        usleep(rand() % 50000 + 1);
+        printf("Smoker TWO with INFINITE PAPER is blowing smoke on blunt number %d\n\n", smokeCount);
+    }
+
+    printf("Smoker TWO with INFINITE PAPER got hungry and started convulsing\n\n");
+    pthread_exit(0);
+}
+
+/* the first smoker with infinite matches */
 void *smokerWithMatchOne(void *param)
 {
     int smokeCount = 0;
@@ -307,15 +399,40 @@ void *smokerWithMatchOne(void *param)
         /* Make a cigarette */
         usleep(rand() % 50000 + 1);
         ++smokeCount;
-        printf("Smoker one with INFINITE MATCHES rolled up blunt number %d\n\n", smokeCount);
+        printf("Smoker ONE with INFINITE MATCHES rolled up blunt number %d\n\n", smokeCount);
         
         sem_post(&agentSem);
 
         /* Smoke the cigarette */
         usleep(rand() % 50000 + 1);
-        printf("Smoker one with INFINITE MATCHES is blowing smoke on blunt number %d\n\n", smokeCount);
+        printf("Smoker ONE with INFINITE MATCHES is blowing smoke on blunt number %d\n\n", smokeCount);
     }
 
-    printf("Smoker one with INFINITE MATCHES got hungry and started eating the rolling paper\n\n");
+    printf("Smoker ONE with INFINITE MATCHES got hungry and started eating the rolling paper\n\n");
+    pthread_exit(0);
+}
+
+/* the second smoker with infinite matches */
+void *smokerWithMatchTwo(void *param)
+{
+    int smokeCount = 0;
+
+    while (smokeCount < 3) 
+    {
+        sem_wait(&matchSem);
+       
+        /* Make a cigarette */
+        usleep(rand() % 50000 + 1);
+        ++smokeCount;
+        printf("Smoker TWO with INFINITE MATCHES rolled up blunt number %d\n\n", smokeCount);
+        
+        sem_post(&agentSem);
+
+        /* Smoke the cigarette */
+        usleep(rand() % 50000 + 1);
+        printf("Smoker TWO with INFINITE MATCHES is blowing smoke on blunt number %d\n\n", smokeCount);
+    }
+
+    printf("Smoker TWO with INFINITE MATCHES got hungry and ate a granola bar\n\n");
     pthread_exit(0);
 }
